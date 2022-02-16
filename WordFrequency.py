48e456b702bf59f6a9643fb38ecf05743e25a92a
Michael-Tinker <michaelt19@protonmail.com>
2022-02-16 13:37:04 -0600

    Completed WorldSeries, still need to finish freq.

---
+++ WordFrequency.py
@@ -0,0 +1,5 @@
+#7) WordFrequency.py - Write a program that reads the contents of a text file (you can use this file - AI.txt Download AI.txt ). 
+#The program should create a dictionary in which the keys are the individual words found in the file and the values are the number of times each word appears. 
+# For example, if the word “the” appears 128 times, the dictionary would contain an element with 'the' as the key and 128 as the value. 
+# The program should display the frequency of each word.
+

80ea0489b77a3cb6cf2de0ba7982debc86962e76
Michael-Tinker <michaelt19@protonmail.com>
2022-02-16 23:23:52 -0600

    FInished WordFrequency.py

--- WordFrequency.py
+++ WordFrequency.py
@@ -3,3 +3,34 @@
 # For example, if the word “the” appears 128 times, the dictionary would contain an element with 'the' as the key and 128 as the value. 
 # The program should display the frequency of each word.
 
+def main():
+    infile = open("AI.txt", "r")
+    words = infile.read()
+    no_special = words.replace("(", "")
+    no_special = no_special.replace(")", "")
+    no_special = no_special.replace('"', "")
+    no_special = no_special.replace(",", "")
+    no_special = no_special.replace(".", "")
+    no_special = no_special.replace("'", "")
+    no_special = no_special.replace("-", "")
+    no_special = no_special.replace("]", "")
+    no_special = no_special.replace("[", "")
+    no_special = no_special.replace("  ", " ")
+
+    lower_case = no_special.lower()
+    AIwords = lower_case.split(" ")
+    FreqDict = {}
+
+    for word in AIwords:
+        if word in FreqDict:
+            FreqDict[word] += 1
+        else:
+            FreqDict[word] = 1
+
+    for key in FreqDict:
+        print(key, ": ", FreqDict[key])
+
+    #Close the File
+    infile.close()
+
+main()
